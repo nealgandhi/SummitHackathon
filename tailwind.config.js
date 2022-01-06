dabcc96c45a5d08a3f0c1ce0d52014aa7d1083b7
Megan Vu <megankimvu@gmail.com>
2022-01-05 23:58:52 -0600

    Revert "color palette updated"
    This reverts commit 47584ed51988d508a03abb54e38c171620c78455.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    colors: {
-      transparent: "transparent",
-      "pale-cerulean": "99C1DE",
-      "beau-blue": "BCD4E6",
-      "alice-blue": "D6E2E9",
-    },
     extend: {},
   },
   plugins: [],
