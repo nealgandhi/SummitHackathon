47018af6b2ea7d69df0c5bd495a9043a75f51b4b
salmanh123 <salmanh084@gmail.com>
2022-01-06 16:37:36 -0600

    added AWS amplify as seen by AWSLogin.js and NavLogin.js which showsa link to clikc to ge the login page

--- src/components/AWSLogin.js
+++ src/components/AWSLogin.js
@@ -9,7 +9,7 @@ function AWSLogin({ signOut, user }) {
   return (
     <>
     
-      <h1>Hello {user.username}</h1>
+      <h1>Hello User</h1>
       <button onClick={signOut}>Sign out</button>
     </>
   );
