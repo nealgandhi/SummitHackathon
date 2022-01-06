6f5bfb1233d52311b6d257524938ad62f560fbca
Salman Jaher <salmanjaher1@gmail.com>
2022-01-06 17:42:33 -0600

    merge jane + sback
    merge jane + sback

--- src/components/AWSLogin.js
+++ src/components/AWSLogin.js
@@ -2,13 +2,12 @@ import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
-import awsExports from 'C:/Users/salma/SummitHackathon/src/aws-exports.js';
+import awsExports from '../../src/aws-exports';
 Amplify.configure(awsExports);
 
 function AWSLogin({ signOut, user }) {
   return (
     <>
-    
       <h1>Hello User</h1>
       <button onClick={signOut}>Sign out</button>
     </>
