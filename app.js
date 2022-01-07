ff2467c76310709465398f117656aa4d2670ae97
nealgandhi <gandhi.neal2002@gmail.com>
2022-01-06 21:12:46 -0600

    Node.js Arduino handler

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const SerialPort = require("serialport");
+const Readline = require("@serialport/parser-readline");
+    
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+app.post("/post", (req, res) => {
+    console.log("Connected to React");
+    res.redirect("/");
+  });
+
+const port = new SerialPort('/dev/cu.SLAB_USBtoUART',{
+    baudRate: 115200
+})
+  
+  async function run(){
+      port.on('error', function(err){
+      console.log('Error: ', err.message)
+      })
+  
+      port.write('0\n', function(err){
+      if(err){
+          return console.log('Error on write: ', err.message)
+      }
+          console.log('blank')
+      })
+      while (true){
+          for (let i =1; i < 25; i++){
+              let amount = `${i}\n`;
+              port.write(amount, function(err){
+                  if(err){
+                      return console.log('Error on write: ', err.message)
+                  }
+                  console.log(amount)
+              })
+              await sleep(100);
+            }
+        }
+}
+
+  const PORT = process.env.PORT || 8080;
+  app.listen(PORT, console.log(`Server started on port ${PORT}`));
+  run();
