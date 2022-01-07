a0c34e7a70be643fe9e44f21204f1f636e5ff8f7
Megan Vu <megankimvu@gmail.com>
2022-01-06 22:01:19 -0600

    updated text on landing page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,20 +1,19 @@
-import React from 'react';
+import React from "react";
 
 export default function Hero() {
   return (
     <div>
-      <div class='pt-24 bg-herobg bg-cover bg-gradient-to-r from-aliceBlue to-beauBlue pb-4 object-scale-down h-screen'>
-        <div class='container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center '>
-          <div class='flex flex-col w-full md:w-2/5 justify-center items-start text-center md: text-left ml-8'>
-            <h1 class='my-4 text-5xl font-bold leading-tight'>
-              Main Hero Message to sell yourself!
+      <div class="pt-24 bg-herobg bg-cover bg-gradient-to-r from-aliceBlue to-beauBlue pb-4 object-scale-down h-screen">
+        <div class="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center ">
+          <div class="flex flex-col w-full md:w-2/5 justify-center items-center text-center md: text-left ml-8">
+            <h1 class="my-4 text-5xl font-bold leading-tight">
+              Manage your money all in one easy place!
             </h1>
-            <p class='leading-normal text-2xl mb-8'>
-              Sub-hero message, not too long and not too short. Make it just
-              right!
+            <p class="leading-normal text-2xl mb-8">
+              Get started with your budget tracking today!
             </p>
-            <button class='mx-auto lg:mx-0 hover:underline bg-paleCerulean text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out'>
-              Log In
+            <button class="mx-auto lg:mx-0 hover:underline bg-paleCerulean text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+              Sign up/Log in
             </button>
           </div>
         </div>
--- src/components/InfoCards.js
+++ src/components/InfoCards.js
@@ -6,7 +6,7 @@ export default function InfoCards() {
       <section class="bg-isabelline border-b py-8">
         <div class="container mx-auto flex flex-wrap pt-4 pb-12">
           <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center">
-            Title
+            Packed with features for your convenience!
           </h1>
           <div class="w-full mb-4 ">
             <div class="h-1 mx-auto bg-gradient-to-r from-aliceBlue to-beauBlue w-64 opacity-25 my-0 py-0 rounded-t"></div>
@@ -17,67 +17,58 @@ export default function InfoCards() {
                 href="#"
                 class="flex flex-wrap no-underline hover:no-underline"
               >
-                <div class="w-full font-bold text-xl px-6 mt-4">
-                  Lorem ipsum dolor sit amet.
+                <div class="w-full font-bold text-xl px-6 mt-4 pb-4 ">
+                  Track your expenses with ease!
                 </div>
                 <p class="text-base px-6 mb-5">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.
+                  Keeping track of your spending couldn't be easier! Just enter
+                  the category and dollar amount for your purchase and your
+                  budget for the last month with be ready just like that!
                 </p>
               </a>
             </div>
             <div class="flex-none mt-auto bg-aliceBlue rounded-b rounded-t-none overflow-hidden shadow p-6">
-              <div class="flex items-center justify-start">
-                <button class="bg-paleCerulean mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                  Action
-                </button>
-              </div>
+              <div class="flex items-center justify-start"></div>
             </div>
           </div>
           <div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
-            <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+            <div class="flex-1 bg-aliceBlue rounded-t rounded-b-none overflow-hidden shadow">
               <a
                 href="#"
                 class="flex flex-wrap no-underline hover:no-underline"
               >
-                <div class="bg-aliceBlue w-full font-bold text-xl text-gray-800 px-6 pt-4">
-                  Lorem ipsum dolor sit amet.
+                <div class="bg-aliceBlue w-full font-bold text-xl text-gray-800 px-6 mt-4 pb-4">
+                  Access your budget anywhere!
                 </div>
                 <p class="bg-aliceBlue text-gray-800 text-base px-6 pb-5">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.
+                  Every time you enter an expense, a pie chart will update with
+                  your expense distribution. And with built-in ring light
+                  integration , you'll be able to check on your spending even
+                  when you're not at your computer!
                 </p>
               </a>
             </div>
-            <div class="flex-none mt-auto bg-aliceBlue rounded-b rounded-t-none overflow-hidden shadow p-6">
-              <div class="flex items-center justify-center">
-                <button class="bg-paleCerulean mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                  Action
-                </button>
-              </div>
-            </div>
+            <div class="flex-none mt-auto bg-aliceBlue rounded-b rounded-t-none overflow-hidden shadow p-6"></div>
           </div>
           <div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
-            <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+            <div class="flex-1 bg-aliceBlue rounded-t rounded-b-none overflow-hidden shadow">
               <a
                 href="#"
                 class="flex flex-wrap no-underline hover:no-underline"
               >
-                <div class="bg-aliceBlue w-full font-bold text-xl text-gray-800 px-6 pt-4">
-                  Lorem ipsum dolor sit amet.
+                <div class="bg-aliceBlue w-full font-bold text-xl text-gray-800 px-6 pt-4 pb-4">
+                  Keep your personal information safe!
                 </div>
                 <p class="text-gray-800 bg-aliceBlue text-base px-6 pb-5">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.
+                  With AWS authentication, your data is easily accessible, but
+                  only to you. Everything in your account is locked behind
+                  encryption for maximum security. Your personal financial data
+                  will be safe and sound!
                 </p>
               </a>
             </div>
             <div class="flex-none mt-auto bg-aliceBlue rounded-b rounded-t-none overflow-hidden shadow p-6">
-              <div class="flex items-center justify-end">
-                <button class="bg-paleCerulean mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                  Action
-                </button>
-              </div>
+              <div class="flex items-center justify-end"></div>
             </div>
           </div>
         </div>
