690655175fc1013a876c3948ca407d7c879714e2
Salman Jaher <salmanjaher1@gmail.com>
2022-01-06 21:50:13 -0600

    updated backend

--- src/App.js
+++ src/App.js
@@ -4,17 +4,16 @@ import './styles/main.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import CategoryList from './components/CategoryList'
 import Login from './components/Login.js';
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
-import awsExports from "./aws-exports";
-import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react'
+import awsExports from './aws-exports';
+import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import AWSLogin from './components/AWSLogin.js'
-import NavLogin from './components/NavLogin.js'
+import AWSLogin from './components/AWSLogin.js';
+import NavLogin from './components/NavLogin.js';
 //Amplify.configure(awsconfig)
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CategoryList from './components/CategoryList';
 
 function App() {
@@ -30,13 +29,13 @@ function App() {
       <Footer />
 
       <div className='App'>
-      <Router>
-        <Routes>
-          <Route path='/' element={<AWSLogin/>} />
-          <Route path='/' element={<NavLogin/>} />
-        </Routes>
-      </Router>
-    </div>
+        <Router>
+          <Routes>
+            <Route path='/' element={<AWSLogin />} />
+            <Route path='/' element={<NavLogin />} />
+          </Routes>
+        </Router>
+      </div>
     </>
   );
 }
